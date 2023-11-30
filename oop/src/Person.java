07140225a67a1fdcd3105b1b18cfd22ecb301ad7
diyorbek0207 <140248878+diyorbek0207@users.noreply.github.com>
2023-11-30 13:03:03 +0500

    add person

--- oop/src/Person.java
+++ oop/src/Person.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Person {
+public class Person {
+    private String ismi;
+    private String familiyasi;
+    private int yoshi;
+
+    public Person(String ismi,String familiyasi,int yoshi){
+        this.ismi=ismi;
+        this.familiyasi=familiyasi;
+        this.yoshi=yoshi;
+    }
+
 }
+
