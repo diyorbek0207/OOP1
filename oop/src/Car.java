e1fb327b57609d4c6bb4c2268bca62bc8626d062
diyorbek0207 <140248878+diyorbek0207@users.noreply.github.com>
2023-11-30 13:01:35 +0500

    add car

---
+++ oop/src/Car.java
@@ -0,0 +1,15 @@
+public class Car {
+        private String color;
+        private String modeli;
+        private int prise;
+        private String tizim;
+
+        public void car(String color, String model,int prise,String tizim){
+            this.color=color;
+            this.modeli=model;
+            this.prise=prise;
+            this.tizim=tizim;
+        }
+
+
+}
