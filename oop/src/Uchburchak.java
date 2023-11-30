d47a135cb1a30c2502ee8326b22782699a97d384
diyorbek0207 <140248878+diyorbek0207@users.noreply.github.com>
2023-11-30 13:04:27 +0500

    add uchburchak

--- oop/src/Uchburchak.java
+++ oop/src/Uchburchak.java
@@ -1,3 +1,12 @@
 public class Uchburchak {
-    
+    private int a;
+    private int b;
+    private int c;
+
+    public Uchburchak(int a,int b,int c){
+        this.a=a;
+        this.b=b;
+        this.c=c;
+
+    }
 }
